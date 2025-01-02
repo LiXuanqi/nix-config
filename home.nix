93ec1dcd4bdf9579e02890be9a90b69dfe38de12
1_x7 <lixuanqi1995@gmail.com>
2025-01-01 22:10:48 -0800

    init

---
+++ home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ...}: {
+  home.username = "lixuanqi";
+  home.homeDirectory = "/home/lixuanqi";
+  home.stateVersion = "24.11";
+
+  home.file.".config" = {
+    source = ./dotfiles/.config;
+    recursive = true;
+  };
+
+  programs.home-manager.enable = true;
+  programs.neovim.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "1_x7";
+    userEmail = "lixuanqi1995@gmail.com";
+  };
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.zsh = {
+    enable = true;
+    # enableCompletion = true;
+    # autosuggestion.enable = true;
+    # syntaxHighlighting.enable = true;
+    # history.size = 10000;
+    # history.ignoreAllDups = true;
+    # history.path = "$HOME/.zsh_history";
+  };
+  programs.starship = {
+    enable = true;
+    # settings = {};
+  };
+}
